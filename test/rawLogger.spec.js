6bb58e99a6dc07c13e05414f99152f4d144813bf
Iiro Jäppinen <iiro.jappinen@verkkokauppa.com>
2019-07-09 07:35:28 +0300

    test: add some tests

---
+++ test/rawLogger.spec.js
@@ -0,0 +1,26 @@
+const sinon = require('sinon')
+const testdouble = require('testdouble')
+const test = require('tape')
+
+const request = sinon.fake.resolves('ok')
+testdouble.replace('request-promise', request)
+
+const { rawLogger } = require('../')
+
+test('rawLogger', assert => {
+    assert.plan(2)
+
+    rawLogger({ body: { foo: 'bar' }, webhook: 'https://example.com' })
+
+    assert.true(request.calledOnce, 'called exactly once')
+
+    assert.true(
+        request.calledOnceWith({
+            body: { foo: 'bar' },
+            json: true,
+            method: 'POST',
+            uri: 'https://example.com'
+        }),
+        'called with correct arguments'
+    )
+})
---
+++ test/simpleLogger.spec.js
@@ -0,0 +1,62 @@
+const sinon = require('sinon')
+const testdouble = require('testdouble')
+const test = require('tape')
+
+const rawLogger = sinon.fake()
+testdouble.replace('../lib/rawLogger', rawLogger)
+
+const simpleLogger = require('../lib/simpleLogger')
+
+test('simpleLogger', assert => {
+    assert.plan(4)
+
+    simpleLogger({ message: 'test', webhook: 'https://example.com' })
+
+    assert.true(rawLogger.calledOnce, 'called exactly once')
+
+    assert.true(
+        rawLogger.calledOnceWith({
+            body: {
+                '@type': 'MessageCard',
+                '@context': 'http://schema.org/extensions',
+                text: 'test'
+            },
+            webhook: 'https://example.com'
+        }),
+        'called with correct arguments'
+    )
+
+    rawLogger.resetHistory()
+
+    simpleLogger({
+        links: [{ label: 'label', href: 'href' }],
+        message: 'test',
+        webhook: 'https://example.com'
+    })
+
+    assert.true(rawLogger.calledOnce, 'called exactly once')
+
+    assert.true(
+        rawLogger.calledOnceWith({
+            body: {
+                '@type': 'MessageCard',
+                '@context': 'http://schema.org/extensions',
+                text: 'test',
+                potentialAction: [
+                    {
+                        '@type': 'OpenUri',
+                        name: 'label',
+                        targets: [
+                            {
+                                os: 'default',
+                                uri: 'href'
+                            }
+                        ]
+                    }
+                ]
+            },
+            webhook: 'https://example.com'
+        }),
+        'called with correct arguments'
+    )
+})
